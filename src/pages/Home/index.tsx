4501f09de1567479950aad062099782051ebf91f
Pierre Terrancle <pi.terrancle@gmail.com>
2025-04-30 18:52:14 +0200

    New fetching method

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,36 @@
-import getResponse from '../../assets/scripts/utils.ts'
+import { useEffect, useState } from 'react';
 const apiUrl = import.meta.env.VITE_API_KEY;
 
 function Home() {
-    const blogs = getResponse(apiUrl);
-    return (
-        <div className="homepage">
-            <p>
-                {blogs}
-            </p>
-            <div className="first-block">
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Error fetching data:', error));
+      }, []);
+    
+        if (!data) 
+            return <p>Loading...</p>;
+
+        return (
+            <div className="homepage">
                 <p>
-                    Ici se trouve le premier bloc de texte, sympa non ?
+                    {data}
                 </p>
+                <div className="first-block">
+                    <p>
+                        Ici se trouve le premier bloc de texte, sympa non ?
+                    </p>
+                </div>
+                <div className="second-block">
+                    <p>
+                        Ici se trouve le deuxième bloc de texte, sympa aussi
+                    </p>
+                </div>
             </div>
-            <div className="second-block">
-                <p>
-                    Ici se trouve le deuxième bloc de texte, sympa aussi
-                </p>
-            </div>
-        </div>
-    )
+        )
 }
 
 export default Home
